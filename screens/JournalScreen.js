2efd4d251f3317679a14591d3c4b217537bc1e90
jk1595 <jk1595@messiah.edu>
2020-10-02 17:00:15 +0800

    Created layout of journal page

--- screens/JournalScreen.js
+++ screens/JournalScreen.js
@@ -1,13 +1,28 @@
 import { Ionicons } from '@expo/vector-icons';
 import * as WebBrowser from 'expo-web-browser';
 import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity  } from 'react-native';
 import { RectButton, ScrollView } from 'react-native-gesture-handler';
+import JournalCard from '../components/JournalCard';
 
-export default function JournalScreen() {
+
+export default function JournalScreen(props) {
+  const onPress = () => props.navigation.navigate("HomeScreen");
   return (
     <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-      <OptionButton
+      <Text style={styles.header}>My Journal</Text>
+
+      <JournalCard> </JournalCard>
+
+      <JournalCard></JournalCard>
+
+      <JournalCard></JournalCard>
+
+      <TouchableOpacity style={styles.button} onPress={onPress}>
+        <Text style={styles.addButton}>+</Text>
+      </TouchableOpacity>
+
+      {/* <OptionButton
         icon="md-school"
         label="Read the Expo documentation"
         onPress={() => WebBrowser.openBrowserAsync('https://docs.expo.io')}
@@ -24,7 +39,7 @@ export default function JournalScreen() {
         label="Ask a question on the forums"
         onPress={() => WebBrowser.openBrowserAsync('https://forums.expo.io')}
         isLastOption
-      />
+      /> */}
     </ScrollView>
   );
 }
@@ -46,8 +61,32 @@ function OptionButton({ icon, label, onPress, isLastOption }) {
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: '#00095e',
     flex: 1,
-    backgroundColor: '#fafafa',
+  },
+  header: {
+    fontFamily: 'Poppins-Medium',
+    color: 'white',
+    fontSize: 26,
+    fontWeight: 'bold',
+    paddingHorizontal: 20,
+    paddingVertical: 10
+  },
+  addButton: {
+    color: 'white',
+    fontSize: 25,
+    paddingVertical: 3,
+    paddingHorizontal: 13
+  },
+  button:{
+    backgroundColor: '#00095e',
+    bottom: -15,
+    alignSelf: 'center',
+    width: 44,
+    height: 44,
+    borderRadius: 44/2,
+    borderWidth: 2,
+    borderColor: 'white'
   },
   contentContainer: {
     paddingTop: 15,
@@ -70,5 +109,5 @@ const styles = StyleSheet.create({
     fontSize: 15,
     alignSelf: 'flex-start',
     marginTop: 1,
-  },
+  }
 });
