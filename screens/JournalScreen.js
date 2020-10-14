afe9cfe336f7f8e0749ae408a7bb39fcdac22962
jk1595 <jk1595@messiah.edu>
2020-10-14 17:04:19 +0800

    Add Journal Entry Button Edit

--- screens/JournalScreen.js
+++ screens/JournalScreen.js
@@ -11,8 +11,11 @@ export default function JournalScreen(props) {
   const onPress = () => props.navigation.navigate("HomeScreen");
 
   return (
-    <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-      <Text style={styles.header}>My Journal</Text>
+
+    <View style={styles.container}>
+      <View style={styles.contentContainer}> 
+         <ScrollView>
+              <Text style={styles.header}>My Journal</Text>
       {
         !!user.journal && Object.values(user.journal).length > 0 ? (
           Object.values(user.journal).map((item, key) =>
@@ -23,15 +26,39 @@ export default function JournalScreen(props) {
           </View>
         )
       }
-      <View style={styles.buttonView}>
-        <TouchableOpacity style={styles.button} onPress={() => props.navigation.navigate('JournalEntry')}>
-          <AntDesign name="pluscircleo" size={40} color="white" /> 
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={onPress}>
-          <FontAwesome name="trash-o" size={40} color="white" />
-        </TouchableOpacity>
+          </ScrollView>
       </View>
-    </ScrollView>
+      {/* <View style={styles.pic} >
+          <ImageBackground style={styles.img}  
+                source={require('../assets/images/shapeDesign2.png')} resizeMode="contain">
+          </ImageBackground>
+       </View> */}
+
+       {/* <View style={styles.pic2} >
+          <ImageBackground style={styles.img}  
+                source={require('../assets/images/shapeDesign3.png')} resizeMode="contain">
+          </ImageBackground>
+       </View> */}
+
+      
+        <View style={styles.buttonView}>
+           <TouchableOpacity style={styles.button1} onPress={() => props.navigation.navigate('JournalEntry')}>
+               <Image style={ styles.imgBackground }  
+                     source={require('../assets/images/pencilTip.png')}> 
+               </Image>
+           </TouchableOpacity>
+      </View>
+
+       <View style = {styles.buttonView2}>
+            <TouchableOpacity style={styles.button} onPress={onPress}>
+                 <FontAwesome name="trash-o" size={40} color="white" />
+            </TouchableOpacity>
+       </View>
+
+        
+      </View>
+    
+
   );
 }
 
@@ -51,8 +78,33 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     paddingVertical: 10
   },
+  pic: {
+    flex: 1,
+    flexDirection: 'row',
+    bottom: '30%',
+  },
+  pic2: {
+    flex: 1,
+    flexDirection: 'row-reverse',
+  },
+  contentContainer: {
+    paddingTop: 15,
+    flex: 1
+  },
+  button1:{
+    backgroundColor: 'white',
+    width: 55,
+    height: 55,
+    borderRadius: 55/2,
+    flexBasis: 'column',
+    borderColor: 'white'
+  },
+  button2: {
+    flexBasis: 'column'
+
+  },
   textContainer: {
-    backgroundColor: '#132090',
+    backgroundColor: 'white',
     justifyContent: 'center',
     alignContent: 'center',
     width: '80%',
@@ -62,16 +114,41 @@ const styles = StyleSheet.create({
     bottom: 10,
     marginTop: 20
   },
+  img: {
+    width: '50%',
+    height: '50%',
+  },
+  imgBackground: {
+    width: '70%',
+    height: '70%',
+    bottom: -7,
+    right: -7
+},
+buttonView: {
+  flex: 1,
+  flexDirection: 'row-reverse',
+  right: '8%',
+  bottom: '3%',
+  position: 'absolute'
+},
+buttonView2: {
+    flex: 1,
+    bottom: '4%',
+    left: '8%',
+    position: 'absolute',
+    //flexDirection: 'row'
+},
   text: {
     textAlign: 'center',
-    color: 'white',
+    color: '#00095e',
     fontFamily: 'Poppins-extra-light',
+    fontWeight: 'bold',
     fontSize: 20,
   },
-  button: {
-    alignSelf: 'center',
-    marginTop: 20,
-  },
+  // button: {
+  //   alignSelf: 'center',
+  //   marginTop: 20,
+  // },
   contentContainer: {
     paddingTop: 15,
     flex: 1
