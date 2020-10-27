93bdd2d80c77adfa8ff75ba71295c5235ba5597b
Drew Weaver <awweaver27@gmail.com>
2020-10-27 09:09:04 -0400

    Beginning of Quiz Modal for reference

--- screens/LikesScreen.js
+++ screens/LikesScreen.js
@@ -1,60 +1,76 @@
 import * as WebBrowser from 'expo-web-browser';
 import * as React from 'react';
-import { Image, Platform, StyleSheet, Text, TouchableOpacity, View , ScrollView} from 'react-native';
+import { Image, Platform, StyleSheet, Text, TouchableOpacity, View, ScrollView } from 'react-native';
 import albumImage from '../assets/images/album-placeholder.png';
 import WelcomeBanner from '../components/WelcomeBanner';
+import {Portal, Modal} from 'react-native-paper';
 
 export default function LikesScreen() {
+
+  const [visible, setVisible] = React.useState(false);
+
+  const showModal = () => setVisible(true);
+
+  const hideModal = () => setVisible(false);
+
   return (
-  <View style={styles.container}>
-    <Text style={styles.header2}>My History</Text>
-    <ScrollView>
-      <View style={styles.textContainer}>
-        <Text style={styles.header}>Give these suggestions a review!</Text>
-        <View style={styles.recentTab}>
-          <Image source={albumImage} style={styles.albumImages}></Image>
-          <Image source={albumImage} style={styles.albumImages}></Image>
-          <Image source={albumImage} style={styles.albumImages}></Image>
+    <View style={styles.container}>
+      <Text style={styles.header2}>My History</Text>
+      <ScrollView>
+        <View style={styles.textContainer}>
+          <Text style={styles.header}>Give these suggestions a review!</Text>
+          <View style={styles.recentTab}>
+            <Image source={albumImage} style={styles.albumImages}></Image>
+            <Image source={albumImage} style={styles.albumImages}></Image>
+            <Image source={albumImage} style={styles.albumImages}></Image>
+          </View>
+          <TouchableOpacity>
+            <Text style={styles.text}>View More</Text>
+          </TouchableOpacity>
         </View>
-        <TouchableOpacity>
-          <Text style={styles.text}>View More</Text>
-        </TouchableOpacity>
-      </View>
-      <View style={styles.textContainer}>
-        <Text style={styles.header}>Recent Suggestions</Text>
-        <View style={styles.recentTab}>
-          <Image source={albumImage} style={styles.albumImages}></Image>
-          <Image source={albumImage} style={styles.albumImages}></Image>
-          <Image source={albumImage} style={styles.albumImages}></Image>
+        <View style={styles.textContainer}>
+          <Text style={styles.header}>Recent Suggestions</Text>
+          <View style={styles.recentTab}>
+            <Portal>
+              <Modal visible={visible} onDismiss={hideModal}>
+                <Text>Example Modal</Text>
+              </Modal>
+            </Portal>
+            <TouchableOpacity onPress={showModal}>
+                <Image source={albumImage} style={styles.albumImages}></Image>
+              </TouchableOpacity>
+            <Image source={albumImage} style={styles.albumImages}></Image>
+            <Image source={albumImage} style={styles.albumImages}></Image>
+            <Image source={albumImage} style={styles.albumImages}></Image>
+          </View>
+          <TouchableOpacity>
+            <Text style={styles.text}>View More</Text>
+          </TouchableOpacity>
         </View>
-        <TouchableOpacity>
-          <Text style={styles.text}>View More</Text>
-        </TouchableOpacity>
-      </View>
-      <View style={styles.textContainer}>
-        <Text style={styles.header}>Recently Liked Suggestions</Text>
-        <View style={styles.recentTab}>
-          <Image source={albumImage} style={styles.albumImages}></Image>
-          <Image source={albumImage} style={styles.albumImages}></Image>
-          <Image source={albumImage} style={styles.albumImages}></Image>
+        <View style={styles.textContainer}>
+          <Text style={styles.header}>Recently Liked Suggestions</Text>
+          <View style={styles.recentTab}>
+            <Image source={albumImage} style={styles.albumImages}></Image>
+            <Image source={albumImage} style={styles.albumImages}></Image>
+            <Image source={albumImage} style={styles.albumImages}></Image>
+          </View>
+          <TouchableOpacity>
+            <Text style={styles.text}>View More</Text>
+          </TouchableOpacity>
         </View>
-        <TouchableOpacity>
-          <Text style={styles.text}>View More</Text>
-        </TouchableOpacity>
-      </View>
-      <View style={styles.textContainer}>
-        <Text style={styles.header}>Most Frequent Suggestions</Text>
-        <View style={styles.recentTab}>
-          <Image source={albumImage} style={styles.albumImages}></Image>
-          <Image source={albumImage} style={styles.albumImages}></Image>
-          <Image source={albumImage} style={styles.albumImages}></Image>
+        <View style={styles.textContainer}>
+          <Text style={styles.header}>Most Frequent Suggestions</Text>
+          <View style={styles.recentTab}>
+            <Image source={albumImage} style={styles.albumImages}></Image>
+            <Image source={albumImage} style={styles.albumImages}></Image>
+            <Image source={albumImage} style={styles.albumImages}></Image>
+          </View>
+          <TouchableOpacity>
+            <Text style={styles.text}>View More</Text>
+          </TouchableOpacity>
         </View>
-        <TouchableOpacity>
-          <Text style={styles.text}>View More</Text>
-        </TouchableOpacity>
-      </View>
-      
-    </ScrollView>
+
+      </ScrollView>
     </View>
   );
 }
@@ -64,16 +80,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#00095e',
     flex: 1
   },
-  header2:{
+  header2: {
     fontFamily: 'Poppins-Medium',
     color: 'white',
     fontSize: 26,
     fontWeight: 'bold',
-    paddingHorizontal:20,
+    paddingHorizontal: 20,
     paddingVertical: '5%',
     marginTop: '7.8%'
   },
-  header:{
+  header: {
     padding: 15,
     fontFamily: 'Poppins-Extra-Light',
     fontSize: 20,
@@ -83,7 +99,7 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '20%'
   },
-  recentTab:{
+  recentTab: {
     flexDirection: 'row',
     justifyContent: 'space-around'
   },
@@ -92,7 +108,7 @@ const styles = StyleSheet.create({
     width: 100,
     height: 100,
   },
-  buttonContainer:{
+  buttonContainer: {
     margin: 10,
     padding: 10
   },
@@ -100,7 +116,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#132090',
     marginBottom: 20,
   },
-  button:{
+  button: {
     marginBottom: 20,
     backgroundColor: '#132090',
     alignSelf: 'center',
@@ -109,9 +125,9 @@ const styles = StyleSheet.create({
   },
   text: {
     color: 'white',
-    fontSize: 16,   
+    fontSize: 16,
     fontFamily: 'Poppins-Extra-Light',
     padding: 15
   },
-  
+
 });
