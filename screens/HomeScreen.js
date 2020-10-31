2be3fc71e34d06cfd27cbcd50033fe2714bfc6d2
wn1154 <43796180+wn1154@users.noreply.github.com>
2020-10-31 15:04:01 -0400

    Added Modal to homescreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,13 +2,22 @@ import * as WebBrowser from 'expo-web-browser';
 import * as React from 'react';
 import { Image, Platform, ScrollView, StyleSheet, Text, TouchableOpacity, View, Button, ImageBackground } from 'react-native';
 import { AuthContext } from '../AuthContext.js';
-
+import { Portal, Modal } from 'react-native-paper';
 import TipOTD from '../components/TipOTD';
 import WelcomeBanner from '../components/WelcomeBanner';
 import albumImage from '../assets/images/album-placeholder.png';
+import { RFValue } from "react-native-responsive-fontsize";
+import Slider from 'react-slick';
+import MagicSliderDots from 'react-magic-slider-dots';
 
 export default function HomeScreen(props) {
   const user = React.useContext(AuthContext);
+
+  const [visible, setVisible] = React.useState(false);
+
+  const showModal = () => setVisible(true);
+
+  const hideModal = () => setVisible(false);
 
   const TOTD = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pulvinar pellentesque ex at maximus. Nam feugiat rhoncus accumsan. ';
   return (
@@ -27,7 +36,10 @@ export default function HomeScreen(props) {
       </View>
       <Text style={styles.header}>Recently Liked</Text>
       <View style={styles.recentTab}>
-        <Image source={albumImage} style={styles.albumImages}></Image>
+        <TouchableOpacity onPress={showModal}>
+          <Image source={albumImage} style={styles.albumImages}></Image>
+        </TouchableOpacity>
+        
         <Image source={albumImage} style={styles.albumImages}></Image>
         <Image source={albumImage} style={styles.albumImages}></Image>
       </View>
@@ -37,7 +49,22 @@ export default function HomeScreen(props) {
       </TouchableOpacity>
       <TipOTD TOTD={TOTD}></TipOTD>
       </View>
-        
+      <Portal>
+        <Modal visible={visible} dismissable={false} contentContainerStyle={styles.tourModal}>
+          <Text style={styles.modalHeader}>Welcome to the HitPause family!</Text>
+          <Text style={styles.modalText}>Our goal is to provide each and every user with their own tips and tricks on how to 
+          better deal with their anxiety. Click next to take the virtual tour and get started!</Text>
+          <View style={styles.recentTab}>
+            <TouchableOpacity style={styles.modalButton} onPress={hideModal}>
+              <Text style={styles.modalText}>Close</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.modalButton} onPress={hideModal}>
+              <Text style={styles.modalText}>Next</Text>
+            </TouchableOpacity>
+          </View>
+          <MagicSliderDots dots={true} numDotsToShow={4} dotWidth={30} />;
+        </Modal>
+      </Portal>  
     </ScrollView>
   );
 }
@@ -54,7 +81,7 @@ const styles = StyleSheet.create({
   },
   header:{
     padding: 15,
-    fontFamily: 'Poppins-Light',
+    fontFamily: 'Poppins-Medium',
     fontSize: 20,
     color: 'white'
   },
@@ -62,10 +89,34 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '55%',
   },
-  
+  tourModal:{
+    backgroundColor: '#132090',
+    justifyContent: 'center',
+    alignContent: 'center',
+    width: '80%',
+    alignSelf: 'center',
+    borderRadius: 10,
+    padding: 10,
+    bottom: 10,
+    margin: 30,
+  },
+  modalHeader:{
+    textAlign: 'center',
+    padding: 10,
+    fontFamily: 'Poppins-Light',
+    fontSize: 25,
+    color: 'white'
+  },
+  modalText:{
+    padding: 15,
+    fontFamily: 'Poppins-Extra-Light',
+    fontSize: 15,
+    color: 'white',
+    textAlign: 'center',
+  },
   recentTab:{
     flexDirection: 'row',
-    justifyContent: 'space-around'
+    justifyContent: 'space-around',
   },
   albumImages: {
     borderRadius: 8,
@@ -82,6 +133,11 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     padding: 10,
     borderRadius: 8
+  },
+  modalButton:{
+    backgroundColor: '#00095e',
+    borderRadius: 8,
+    width: RFValue(80),
   },
   text: {
     color: 'white',
