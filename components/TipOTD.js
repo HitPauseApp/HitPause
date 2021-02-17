f32da582c986857177ef322060c0cb5e25a543a3
jk1595 <jk1595@messiah.edu>
2021-02-16 23:21:33 -0500

    Redesigned HomePage and AccountPage

--- components/TipOTD.js
+++ components/TipOTD.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import { StyleSheet, View, Text, Platform } from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
 
 export default class TipOTD extends Component {
     constructor(props) {
@@ -48,9 +49,12 @@ export default class TipOTD extends Component {
         if(Platform.OS === 'ios' || Platform.OS === 'android'){
             return (
                 <View style={styles.container}>
+                    <View style={styles.textContainer}>
                     <Text style={styles.header}>Quote of the Day</Text>
-                    <Text style={styles.bodyText}>"{this.state.jsonData.q}"</Text>
-                    <Text style={styles.bodyText}>- {this.state.jsonData.a}</Text>
+                            <Text style={styles.bodyText}>"{this.state.jsonData.q}"</Text>
+                            <Text style={styles.bodyText}>- {this.state.jsonData.a}</Text>
+                    </View>
+                    
                     {/* <Text style={styles.attributionText}>Inspirational quotes provided by{"\n"}
                         <a href="https://zenquotes.io/" style={{color: 'white'}} target="_blank">ZenQuotes API</a>
                     </Text> */}
@@ -70,37 +74,57 @@ export default class TipOTD extends Component {
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: '#132090',
+        backgroundColor: 'white',
         justifyContent: 'center',
         alignContent: 'center',
-        width: '80%',
+        width: '90%',
+        height:'100%',
         alignSelf: 'center',
         borderRadius: 10,
-        padding: 10,
-        bottom: 10,
-        margin: 10
+        //padding: 10,
+        //bottom: 10,
+        //margin: 10
+        //marginTop: '10%',
+        //flex:1
+        shadowColor:  "#000",
+    shadowOffset: {
+    width: RFValue(1),
+    height: RFValue(5),
+  },
+  shadowOpacity: 0.25,
+  shadowRadius: RFValue(3.84),
+  elevation: RFValue(1),
+  borderRadius: RFValue(15),
     },
     header: {
-        color: 'white',
-        fontFamily: 'Poppins-Medium',
+        color: '#00095e',
+        fontFamily: 'Poppins-Bold',
         fontSize: 24,
         textAlign: 'center',
-        textDecorationLine: 'underline'
+        
+       // textDecorationLine: 'underline'
     },
     bodyText: {
-        color: 'white',
-        fontFamily: 'Poppins-Light',
+        color: '#00095e',
+        fontFamily: 'Poppins-Medium',
         marginTop: 15,
-        fontSize: 20,
+        fontSize: RFValue(13),
         textAlign: 'center',
     },
     attributionText: {
-        color: 'white',
+        color: '#00095e',
         fontFamily: 'Poppins-Extra-Light',
         marginTop: 5,
         fontSize: 10,
         textAlign: 'center',
         marginTop: 25
     },
+    textContainer: {
+        width:'89%',
+       height:'89%',
+        alignSelf: 'center',
+        justifyContent:'center'
+        
+    }
 
 });
