d054885ddcf4ee9b26e06e2e88f566a8b26a1320
tannerstern <ts1377@messiah.edu>
2021-02-26 11:23:43 -0500

    Cross-platfrom styling fixes for sign up page

--- screens/Login.js
+++ screens/Login.js
@@ -102,7 +102,7 @@ const styles = StyleSheet.create({
   loginForm: {
     backgroundColor: 'white',
     flex: 1,
-    borderRadius: 15,
+    borderRadius: 20,
     padding: 10,
     display: 'flex',
     borderBottomLeftRadius: 0,
